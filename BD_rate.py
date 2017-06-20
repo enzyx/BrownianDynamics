c9e7c2c44e225fcadb8f5d0bb64964461c00d959
Manuel Luitz <manuel.luitz@gmail.com>
2017-06-20 13:47:01 +0200

    Added GPL header to files

--- BD_rate.py
+++ BD_rate.py
@@ -1,5 +1,23 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+#
+# This file is part of BrownianDynamics. 
+# Copyright (C) 2017 Manuel Luitz <manuel.luitz@gmail.com>
+# Copyright (C) 2017 Fabian Zeller <fabian.zeller11@gmail.com>
+#
+# BrownianDynamics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# BrownianDynamics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with BrownianDynamics. If not, see <http://www.gnu.org/licenses/>.
+# 
 import numpy
 import sys
 import argparse
